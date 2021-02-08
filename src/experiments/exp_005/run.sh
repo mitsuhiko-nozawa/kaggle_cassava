fa72274f99a3812e720032e0c7d5f7a0610e8b08
mitsuhiko-nozawa <mitsuhiko.0516.nozawa@gmail.com>
2021-02-08 16:31:03 +0000

    add criterion

--- src/experiments/exp_005/run.sh
+++ src/experiments/exp_005/run.sh
@@ -5,3 +5,5 @@ exp_param.WORK_DIR=$(pwd) \
 exp_param.ROOT=${root} \
 exp_param.exp_name=${name} \
 hydra.run.dir=. hydra.output_subdir=null hydra/job_logging=disabled hydra/hydra_logging=disabled
+
+#nohup sh -c 'while ps -p 40199 > /dev/null; do sleep 60; done; sh run.sh &
